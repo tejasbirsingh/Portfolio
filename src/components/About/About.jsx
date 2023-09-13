ffa31d7912d4742de2da005eeea27fe3c7900edd
tejasbirsingh <tejasbir.rekhi@gmail.com>
2023-09-13 20:59:41 +0530

    Intial portfolio website setup

---
+++ src/components/About/About.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Software Development Engineer</h3>
+              <p>
+                I'm a software developer with experience in designing and working with micro services
+                utilizing AWS services
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs which interact with databases and downstream services
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Tejas Bir Singh</h1>
+        <p className={styles.description}>
+          I'm a software development engineer with 2+ years of experience in working with microservices, AWS services, backend.
+           Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:tejasbir.rekhi@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/myPhoto.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
